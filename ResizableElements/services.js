eac0cd801f11a37842a447a1a183abdb6972533f
John Lane <john.lane@fdmgroup.com>
2021-12-14 14:25:48 -0500

    code clean up

--- ResizableElements/services.js
+++ ResizableElements/services.js
@@ -3,10 +3,8 @@ function addElements(){
     for (let index = 1; index < 9; index++) {
         console.log(index)
 
-       var div = document.createElement('div');
-       div.innerHTML = `${index}`;
-       document.getElementsByClassName('app-container')[0].appendChild(div);
-        
+        let div = document.createElement('div');
+        div.innerHTML = `${index}`;
+        document.getElementsByClassName('app-container')[0].appendChild(div);
     }
-
 }
