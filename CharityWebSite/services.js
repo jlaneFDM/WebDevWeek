bc77533dfcfa6fa45c9294676805ec130b3365f5
gaalabi <ganiyatalabi@gmail.com>
2021-12-16 08:20:39 -0600

    Add files via upload
    Updated code with carousel.

--- CharityWebSite/services.js
+++ CharityWebSite/services.js
@@ -3,24 +3,29 @@ showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-    showSlides(slideIndex += n);
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("slides");
-    
-    if (n > slides.length) {
-        slideIndex = 1;
-    }
-    if (n < 1) {
-        slideIndex = slides.length
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-}
+  var i;
+  var slides = document.getElementsByClassName("slides");
 
-function currentSlide(n) {
-    showSlides(slideIndex = n);
+  
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
   }
+
+  slides[slideIndex-1].style.display = "block";
+
+  }
+
+
+  
